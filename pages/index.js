4b8854d37a90b602e4cbc94e834070205758d76d
Marhatta <vishal.marhatta@gmail.com>
2021-06-08 17:32:41 +0530

    setting up routes for events app

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,7 @@
-import Link from 'next/link';
 function HomePage() {
   return (
     <div>
       <h1>Home page</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-        <li>
-          <Link href="/blog">Blog</Link>
-        </li>
-      </ul>
     </div>
   );
 }
