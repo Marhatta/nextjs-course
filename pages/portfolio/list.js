3330fc22f4721ffbd333d52ce657a823ab36c141
Marhatta <vishal.marhatta@gmail.com>
2021-06-11 11:22:56 +0530

    blog project setup

--- pages/portfolio/list.js
+++
@@ -1,9 +0,0 @@
-function ListPage() {
-  return (
-    <div>
-      <h1>list page</h1>
-    </div>
-  );
-}
-
-export default ListPage;
---
+++ pages/posts/index.js
@@ -0,0 +1,5 @@
+function AllPostsPage() {
+  return <h1>all posts</h1>;
+}
+
+export default AllPostsPage;
